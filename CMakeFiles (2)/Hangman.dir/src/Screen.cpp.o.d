7d2da67ca20a405ea32561f3afcd6e0c092c5e7a
nuzlockPH <tatingramel@gmail.com>
2023-10-04 22:48:01 +0800

    deleted cmakefiles 2

--- CMakeFiles (2)/Hangman.dir/src/Screen.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Hangman.dir/src/Screen.cpp.o: \
-  /storage/emulated/0/Documents/Cxxdroid/cpp/Hangman/src/Screen.cpp \
-  /storage/emulated/0/Documents/Cxxdroid/cpp/Hangman/include/Screen.hpp
